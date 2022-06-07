3f65dc5eced5a9f0dc6de98fd0879bb33136e0e1
harisalsheikh <harisalsheikh@hotmail.com>
2022-06-07 21:52:55 +0800

    first set of python code for API
    API added for GET method

--- Backend/cloudresumeapi.py
+++ Backend/cloudresumeapi.py
@@ -1,6 +1,25 @@
 import boto3
 
+def lambda_handler(event, context):
 
+    ##Create a resource for the table
+    dynamodb = boto3.resource('dynamodb')
+    
+    ##Variable to store the table 
+    table = dynamodb.Table('CloudResumeChallenge')
 
+    ##Get Id from table 
+    response = table.get_item(
+        Key = {
+            'Id': event['Id'],
+            'Counter': event['Counter']
+        }
+    )
+    ##starting with only getting GET response 
+    return {
+        'statusCode' : response['ResponseMetadata']['HTTPStatusCode'], 
+        'body' : 'Record' + event['Id']
+    }
 
+    ##TODO: PUT Response to update DynamoDB
 
