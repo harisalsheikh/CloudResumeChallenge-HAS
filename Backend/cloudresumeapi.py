6121e744fabbbea0b7786d7585f6416d27b6c166
harisalsheikh <harisalsheikh@hotmail.com>
2022-06-08 20:38:01 +0800

    further mods to API
    added paths for API gateway

--- Backend/cloudresumeapi.py
+++ Backend/cloudresumeapi.py
@@ -1,20 +1,29 @@
 import boto3
 
+
+##Create a resource for the table
+dynamodb = boto3.resource('dynamodb')
+    
+##Variable to store the table 
+table = dynamodb.Table('CloudResumeChallenge')
+
+getMethod = 'GET'
+patchMethod = 'PATCH'
+getId = '/getid'
+
 def lambda_handler(event, context):
 
-    ##Create a resource for the table
-    dynamodb = boto3.resource('dynamodb')
-    
-    ##Variable to store the table 
-    table = dynamodb.Table('CloudResumeChallenge')
+    httpMethod = event['httpMethod']
+    path = event['path']
 
     ##Get Id from table 
-    response = table.get_item(
-        Key = {
-            'Id': event['Id'],
-            'Counter': event['Counter']
-        }
-    )
+    if httpMethod == getMethod and path == getId:
+        response = table.get_item(
+            Key = {
+                'Id': event['Id'],
+                'Counter': event['Counter']
+            }
+        )
     ##starting with only getting GET response 
     return {
         'statusCode' : response['ResponseMetadata']['HTTPStatusCode'], 
